b39df4ff5ead258265b57fa060d78410c97be1eb
lzb <liuzibin@timingbar.com>
2022-11-11 17:51:53 +0800

    状态

---
+++ src/layout/components/tag-bar/ScrollPane.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-scrollbar class="tag-scroll-bar" ref="scrollBar" :noresize="true" @scroll="monitorScrollChange">
+      <slot />
+  </el-scrollbar>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// tag间距
+ const tagAndTagSpacing = 10;
+ 
+
+/**
+ * @description: 监听滚动事件
+ */
+const monitorScrollChange = ()=>{
+
+}
+
+</script>
+<style scoped lang='scss'>
+</style>
--- src/layout/components/tag-bar/TagsBar.vue
+++ src/layout/components/tag-bar/TagsBar.vue
@@ -1,16 +1,99 @@
 <template>
   <div class="tag-bar-wrapper">
+    <scroll-pane ref="scrollPane">
 
+      <router-link v-for="tag in visitedViews" :key="tag.path" tag="span"
+        :to="{path: tag.path, query: tag.query,fullPath: tag.fullPath}" class="tag-bar-item"
+        @click.middle="isAffix(tag) ? '': closeSelectedTag(tag)" @contextmenu.prevent="openMenuPane(tag, $event)">
+
+      </router-link>
+
+    </scroll-pane>
   </div>
 </template>
 
 <script setup>
-// 定义属性
-const props = defineProps({});
+import { computed, getCurrentInstance, reactive, watch } from 'vue';
+import ScrollPane from './ScrollPane.vue';
+import { useTagsViewStore } from '../../../store/modules/tagsView.js';
+import { usePermissionStore } from '../../../store/modules/permission';
+import { useRouter, useRoute } from 'vue-router';
 
-// 定义
+const useTags = useTagsViewStore();
+const usePermission = usePermissionStore();
+const $router = useRouter();
+const $route = useRoute();
+const { proxy } = getCurrentInstance();
 
-const emits = defineEmits();
+// ============================================定义属性==================================================//
+const state = reactive({
+  visible: false,
+  top: 0,
+  left: 0,
+  selectedTag: {},
+  currTag: {},
+  affixTags: [],
+});
+const visitedViews = computed(() => useTags.visitedViews);
+const routes = computed(() => usePermission.routes);
+
+// ============================================watch==================================================//
+watch(
+  () => $route.path,
+  (toPath) => {}
+);
+
+watch(
+  () => state.visible,
+  (val) => {
+    if (val) {
+      document.addEventListener('click', closeVisibleMenuPane);
+    } else {
+      document.removeEventListener('click', closeVisibleMenuPane);
+    }
+  }
+);
+
+// ============================================methods==================================================//
+
+/**
+ * @description: 是否是固定标签
+ * @param {*} tag tag标签
+ * @return {*} 是否是固定标签
+ */
+const isAffix = (tag) => tag.meta && tag.meta.affix;
+
+/**
+ * @description: 关闭选择的菜单面板
+ * @param {*} event 事件
+ * @return {*}  无返回
+ */
+const closeVisibleMenuPane = (event) => {
+  const { classList } = event.target;
+  if (classList.contains('tags-view-operate') || classList.contains('el-icon-setting')) {
+    return false;
+  }
+  state.visible = false;
+  return false;
+};
+
+/**
+ * @description: 打开菜单面板
+ * @param {*} tag  标签
+ * @param {*} event 事件
+ * @return {*} void
+ */
+const openMenuPane = (tag, $event) => {
+  // 左边距
+  const offsetLeft = proxy.$el.getBoundingClientRect().left;
+};
+
+/**
+ * @description: 关闭选择的标签
+ * @param {*} tag tag标签
+ * @return {*} void
+ */
+const closeSelectedTag = (tag) => {};
 </script>
 <style scoped lang='scss'>
 </style>
